15f4ce76c67cc181ef6062da4408d56ab2c43032
Daniil <dgilev75@gmail.com>
2024-11-19 11:13:38 +0300

    feat: add bookmark to persPage, navigation to footer, styles

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,18 +1,22 @@
 import { Socials } from '@components/constants';
+import { Nav } from '@components/Nav/Nav';
 
 import styles from './Footer.module.scss';
 
 export const Footer = () => {
   return (
     <footer className={styles.footer}>
-      <div className={styles.socials}>
-        {Socials.map((el, index) => (
-          <a key={index} href={el.href} target="_blank" rel="noreferrer">
-            <img src={el.icon} />
-          </a>
-        ))}
+      <Nav />
+      <div className={styles.info}>
+        <div className={styles.socials}>
+          {Socials.map((el, index) => (
+            <a key={index} href={el.href} target="_blank" rel="noreferrer">
+              <img src={el.icon} />
+            </a>
+          ))}
+        </div>
+        <p>©Make by Daniil</p>
       </div>
-      <p>©Make by Daniil</p>
     </footer>
   );
 };
